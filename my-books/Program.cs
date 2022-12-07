385ca38b9239df8e2ecd02bbafcc7ee8a454c32b
Asatryan Vache <vacheasatryan9403@gmail.com>
2022-12-07 11:37:03 +0400

    new changes

--- my-books/Program.cs
+++ my-books/Program.cs
@@ -1,3 +1,7 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
+using my_books.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -5,15 +9,16 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-
+builder.Services.AddSwaggerGen(x => x.SwaggerDoc("v2", new OpenApiInfo { Title = "my_books_updated", Version = "v2" }));
+//Configure DBContext with SQL
+builder.Services.AddDbContext<AppDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnectionString")));
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseSwaggerUI(x => x.SwaggerEndpoint("/swagger/v2/swagger.json", "my_books_updated v1"));
 }
 
 app.UseHttpsRedirection();
